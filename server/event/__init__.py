ccff5a7b3cfd0d3ad0383be6a09d68959ca1a04a
Magno Martinez <66977118+Magno-12@users.noreply.github.com>
2022-06-29 19:59:26 -0500

    Formating code

--- server/event/__init__.py
+++ server/event/__init__.py
@@ -1,3 +1,4 @@
+from event.metrics.views import catalog
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -13,7 +14,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from event.metrics.views import catalog
 app.register_blueprint(catalog)
 
 db.create_all()
